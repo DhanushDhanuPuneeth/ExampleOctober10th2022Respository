d99bd97514ac8bd09fd1a3bc1e0e30cfd57391af
DhanushDhanuPuneeth <dhanush19052001@gmail.com>
2023-01-02 04:20:48 -0800

    whileloop

---
+++ Web-Automation/src/p1/whileloop.java
@@ -0,0 +1,18 @@
+package p1;
+
+public class whileloop {
+
+	public static void main(String[] args) {
+		int i=0;
+		while(i<40)
+		{
+			if(i%2==0)
+			{
+				System.out.println(i);
+			}
+			i++;
+
+	}
+
+}
+}
